0987e73505e7e20b9120116b97d38b03b9b4bec3
thiciano <thiciano.musica@gmail.com>
2018-02-17 21:52:25 -0200

    Testando a primeira classe de dominio

--- src/main/java/com/br/deubom/resources/CategoriaResource.java
+++ src/main/java/com/br/deubom/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.br.deubom.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.br.deubom.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "O REST ESTA FUNCIONANDO";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Eletrônicos");
+		Categoria cat2 = new Categoria(2, "Moveis");
+		
+		List<Categoria> categosrias = new ArrayList<Categoria>();
+		categosrias.add(cat1);
+		categosrias.add(cat2);
+		
+		
+		return categosrias;
 	}
 	
 }
