f195c3192a21bfdcaf984911e32a82a98016ec12
thiciano <thiciano.musica@gmail.com>
2018-02-17 21:20:46 -0200

    Projeto inicial

---
+++ src/main/java/com/br/deubom/DeuBomApplication.java
@@ -0,0 +1,12 @@
+package com.br.deubom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeuBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeuBomApplication.class, args);
+	}
+}
