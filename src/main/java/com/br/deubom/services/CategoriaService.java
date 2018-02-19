e9aaa77082d9f4a890f8d0121d028349c2a925f3
thiciano <thiciano.musica@gmail.com>
2018-02-19 10:35:46 -0300

    Atualizando Categorias com PUT. Outros pequenos ajustes.

--- src/main/java/com/br/deubom/services/CategoriaService.java
+++ src/main/java/com/br/deubom/services/CategoriaService.java
@@ -13,7 +13,7 @@ public class CategoriaService {
 	@Autowired
 	public CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		
 		Categoria obj = repo.findOne(id);
 		if(obj == null) {
@@ -25,6 +25,11 @@ public class CategoriaService {
 	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);		
+		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());		
 		return repo.save(obj);
 	}
 }
